a16b1d683cdfae93492206d10672800009c597b6
saifulu <saifulu@gmail.com>
2025-09-15 09:10:26 +0700

    menata menu sidebar -blm selesai

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -4,6 +4,12 @@ import PatientController from './PatientController'
 import EmployeeController from './EmployeeController'
 import RegPeriksaController from './RegPeriksaController'
 import RawatJalanController from './RawatJalanController'
+import RawatInapController from './RawatInapController'
+import IGDController from './IGDController'
+import KamarOperasiController from './KamarOperasiController'
+import LaboratoriumController from './LaboratoriumController'
+import RadiologiController from './RadiologiController'
+import RehabilitasiMedikController from './RehabilitasiMedikController'
 
 const Controllers = {
     API: Object.assign(API, API),
@@ -12,6 +18,12 @@ const Controllers = {
     EmployeeController: Object.assign(EmployeeController, EmployeeController),
     RegPeriksaController: Object.assign(RegPeriksaController, RegPeriksaController),
     RawatJalanController: Object.assign(RawatJalanController, RawatJalanController),
+    RawatInapController: Object.assign(RawatInapController, RawatInapController),
+    IGDController: Object.assign(IGDController, IGDController),
+    KamarOperasiController: Object.assign(KamarOperasiController, KamarOperasiController),
+    LaboratoriumController: Object.assign(LaboratoriumController, LaboratoriumController),
+    RadiologiController: Object.assign(RadiologiController, RadiologiController),
+    RehabilitasiMedikController: Object.assign(RehabilitasiMedikController, RehabilitasiMedikController),
 }
 
 export default Controllers
