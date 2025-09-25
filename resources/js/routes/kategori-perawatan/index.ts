11166683127bdbe5a281274b65ed9bb5e0654cf4
saifulu <saifulu@gmail.com>
2025-09-25 19:17:31 +0700

    fix: remove duplicate imports in route files

--- resources/js/routes/kategori-perawatan/index.ts
+++ resources/js/routes/kategori-perawatan/index.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../wayfinder'
 /**
 * @see \App\Http\Controllers\KategoriPerawatanController::index
 * @see app/Http/Controllers/KategoriPerawatanController.php:15
--- resources/js/routes/storage/index.ts
+++ resources/js/routes/storage/index.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../wayfinder'
 /**
 * @see [serialized-closure]:2
 * @route '/storage/{path}'
