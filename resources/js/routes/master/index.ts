d2cb042b34966f6ca2bf983178f484fc9313be12
aripermadi <arisuneo@gmail.com>
2025-11-16 16:58:26 +0700

    Enhance PembayaranController to support JSON responses for ralan and ranap methods; update SidebarMenu for active menu detection; improve PembayaranIndex with loading states and error handling.

---
+++ resources/js/routes/master/index.ts
@@ -0,0 +1,46 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../wayfinder'
+/**
+* @see routes/web.php:79
+* @route '/master'
+*/
+export const alias = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: alias.url(options),
+    method: 'get',
+})
+
+alias.definition = {
+    methods: ["get","head"],
+    url: '/master',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see routes/web.php:79
+* @route '/master'
+*/
+alias.url = (options?: RouteQueryOptions) => {
+    return alias.definition.url + queryParams(options)
+}
+
+/**
+* @see routes/web.php:79
+* @route '/master'
+*/
+alias.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: alias.url(options),
+    method: 'get',
+})
+
+/**
+* @see routes/web.php:79
+* @route '/master'
+*/
+alias.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: alias.url(options),
+    method: 'head',
+})
+
+const master = {
+    alias: Object.assign(alias, alias),
+}
+
+export default master
