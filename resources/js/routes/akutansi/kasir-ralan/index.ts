22c14ee263505685196de4b431eb8b5a3b0c6b12
saifulu <saifulu@gmail.com>
2025-12-18 01:48:10 +0700

    CPPT

--- resources/js/routes/akutansi/kasir-ralan/index.ts
+++ resources/js/routes/akutansi/kasir-ralan/index.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Akutansi\BillingController::page
 * @see app/Http/Controllers/Akutansi/BillingController.php:41
